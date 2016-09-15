d9a253e4d98e143a0434c9808f2bea9efe9567c2
Vladimir Smirnov <funkrailroad@Funks-Mac-Pro.local>
2016-09-14 23:20:48 -0400

    threejune

--- app/canvas-graphics/header-background.js
+++ app/canvas-graphics/header-background.js
@@ -19,10 +19,47 @@ var Cloud = function (o) {
 
   this.cX = o.cX || 0;
   this.cY = o.cY || 0;
-  this.numBalls = Math.trunc(Math.random() * 3 * 2 + 1);
+  this.cloudLength = Math.trunc(Math.random() * 100);
+  this.numBalls = Math.trunc(Math.random() * 3 * 2 + 3);
+  this.balls = [];
+
+  for(let i = 0; i < this.numBalls; i++) {
+    var newBall = {};
+
+    newBall.cXoffset = Math.trunc(Math.random() * this.cloudLength);
+    newBall.cYoffset = Math.trunc(Math.random() * 20);
+    newBall.r = Math.trunc(Math.random() * (this.cloudLength/this.numBalls));
+
+    this.balls.push(newBall);
+  }
 };
+Cloud.prototype.drawCloud = function () {
+  var _this = this;
 
-var cloud1 = new Cloud();
+  for(let i = 0; i < _this.numBalls; i++) {
+    ctx.arc(_this.balls[i].cXoffset + _this.cX, _this.balls[i].cYoffset + _this.cY, _this.balls[i].r, 0, 2*Math.PI);
+    ctx.fillStyle = '#ffffff';
+    ctx.fill();
+  }
+}
 
-console.log(cloud1.numBalls);
+function animate (prevTime) {
+
+  if((Date.now() - prevTime) > 6000) {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = bgGradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    cloud1.cX = cloud1.cX + 1;
+    cloud1.drawCloud();
+  }
+
+  requestAnimationFrame(animate);
+}
+
+var cloud1 = new Cloud({
+  cX: 60,
+  cY: 50
+});
+
+animate(Date.now());
 })();
